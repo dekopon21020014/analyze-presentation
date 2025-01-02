513cffe9f28ca7926ca10e9a620c1a7ec49853c0
dekopon21020014 <110777220+dekopon21020014@users.noreply.github.com>
2025-01-02 12:09:05 +0900

    fix: GPUに対応させる

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -10,9 +10,22 @@ def init_gemini():
     return genai.GenerativeModel("gemini-1.5-flash")
 
 def init_speech_model():
-    os.environ["CUDA_VISIBLE_DEVICES"] = settings.CUDA_VISIBLE_DEVICES
+    import torch
+
+    # GPU が利用可能であれば利用する
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # 環境変数の設定 (特定の GPU を指定する場合)
+    if device == "cuda":
+        os.environ["CUDA_VISIBLE_DEVICES"] = settings.CUDA_VISIBLE_DEVICES
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+    # TEMP_DIR を作成
     os.makedirs(settings.TEMP_DIR, exist_ok=True)
-    return load_model(device="cpu")
+
+    # モデルをロード
+    return load_model(device=device)
 
 gemini_model = init_gemini()
 speech_model = init_speech_model()
